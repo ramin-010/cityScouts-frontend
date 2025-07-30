8eb159c8e1e0bc9a28d2a22b45ca20e8df72b548
ramin-010 <raminchoudhary737@gmail.com>
2025-07-30 20:52:07 +0530

    fixed the vercel routing issue

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,23 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['axios', 'recoil', 'react-icons'],
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
   },
 })
